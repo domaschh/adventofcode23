1c8b78ed7a0c4548a237c36aa9ac8355ce30eb30
Domasch <magesthomas@gmail.com>
2023-12-05 19:10:59 +0100

    dayfive2 bruteforce

--- src/days/dayfive.rs
+++ src/days/dayfive.rs
@@ -1,12 +1,30 @@
-use std::{collections::HashMap, fs::File, io::BufReader, ops::Range};
+use std::{collections::HashMap, fs::File, io::BufReader, ops::Range, process};
 
-use itertools::Itertools;
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 #[derive(Debug)]
 struct Smap {
     from_l: i64,
     to_l: i64,
     range: i64,
+}
+
+impl PartialEq for Smap {
+    fn eq(&self, other: &Self) -> bool {
+        self.from_l.eq(&other.from_l)
+    }
+}
+impl Eq for Smap {}
+impl PartialOrd for Smap {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.from_l.cmp(&other.from_l))
+    }
+}
+
+impl Ord for Smap {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.from_l.cmp(&other.from_l)
+    }
 }
 
 pub(crate) fn dayfive1(filename: &str) -> Result<i64, String> {
@@ -20,12 +38,13 @@ pub(crate) fn dayfive1(filename: &str) -> Result<i64, String> {
     let seeds = inputlines
         .first()
         .map(|string| {
-            string
-                .split(" ")
-                .into_iter()
-                .skip(1)
-                .filter_map(|s| s.parse::<i64>().ok())
-                .collect_vec()
+            itertools::Itertools::collect_vec(
+                string
+                    .split(" ")
+                    .into_iter()
+                    .skip(1)
+                    .filter_map(|s| s.parse::<i64>().ok()),
+            )
         })
         .ok_or("seedparsing failed")?;
     let mut lastmap = "seeds".to_string();
@@ -122,20 +141,121 @@ fn parse_line_to_map(input: &str) -> Smap {
     }
 }
 
-pub(crate) fn dayfive2(filename: &str) -> std::io::Result<u32> {
-    use std::io::BufRead;
-    let file = File::open(filename)?;
-    let reader: Vec<String> = BufReader::new(file)
-        .lines()
-        .filter_map(Result::ok)
-        .collect();
-
-    Ok(1)
+fn parse_line_to_reverse_map(input: &str) -> Smap {
+    let mut spliterator = input.split(" ");
+    Smap {
+        from_l: spliterator
+            .next()
+            .and_then(|str| str.parse::<i64>().ok())
+            .unwrap_or(0),
+        to_l: spliterator
+            .next()
+            .and_then(|str| str.parse::<i64>().ok())
+            .unwrap_or(0),
+        range: spliterator
+            .next()
+            .and_then(|str| str.parse::<i64>().ok())
+            .unwrap_or(0),
+    }
 }
 
-#[test]
+pub(crate) fn dayfive2(filename: &str) -> Result<i64, String> {
+    use std::io::BufRead;
+    let file = File::open(filename).map_err(|_| "Sa")?;
+    let inputlines: Vec<String> = BufReader::new(file)
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|l| !l.is_empty())
+        .collect();
+    let seeds: Vec<i64> = inputlines
+        .first()
+        .map(|string| {
+            string
+                .split(" ")
+                .into_iter()
+                .skip(1)
+                .filter_map(|s| s.parse::<i64>().ok())
+        })
+        .ok_or("dasd")?
+        .collect();
+    let pairs: Vec<_> = seeds
+        .chunks_exact(2)
+        .map(|chunk| {
+            let from = chunk[0];
+            let to = chunk[0] + chunk[1];
+            [from, to]
+        })
+        .collect();
+    let mut lastmap = "seeds".to_string();
+    let mut ssm: Vec<Smap> = vec![];
+    let mut sfm: Vec<Smap> = vec![];
+    let mut fwm: Vec<Smap> = vec![];
+    let mut wlm: Vec<Smap> = vec![];
+    let mut ltm: Vec<Smap> = vec![];
+    let mut thm: Vec<Smap> = vec![];
+    let mut hlm: Vec<Smap> = vec![];
+    for line in inputlines {
+        match &*line {
+            m @ "seed-to-soil map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "soil-to-fertilizer map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "fertilizer-to-water map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "water-to-light map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "light-to-temperature map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "temperature-to-humidity map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            m @ "humidity-to-location map:" => {
+                lastmap = m.to_string();
+                continue;
+            }
+            _ => {}
+        };
+        match &*lastmap {
+            "seed-to-soil map:" => ssm.push(parse_line_to_map(&line)),
+            "soil-to-fertilizer map:" => sfm.push(parse_line_to_map(&line)),
+            "fertilizer-to-water map:" => fwm.push(parse_line_to_map(&line)),
+            "water-to-light map:" => wlm.push(parse_line_to_map(&line)),
+            "light-to-temperature map:" => ltm.push(parse_line_to_map(&line)),
+            "temperature-to-humidity map:" => thm.push(parse_line_to_map(&line)),
+            "humidity-to-location map:" => hlm.push(parse_line_to_map(&line)),
+            _ => {}
+        }
+    }
 
-fn testmap() {
-    let mut rangemap: HashMap<Range<u32>, Range<u32>> = HashMap::new();
-    println!("Found {:?}", rangemap.get(&(7..7)));
+    let min = pairs
+        .par_iter()
+        .flat_map(|&range| {
+            (range[0]..range[1])
+                .into_par_iter()
+                .map(|seed| {
+                    let soil = find_in_map(&ssm, seed);
+                    let fertilizer = find_in_map(&sfm, soil);
+                    let water = find_in_map(&fwm, fertilizer);
+                    let light = find_in_map(&wlm, water);
+                    let temperature = find_in_map(&ltm, light);
+                    let humidity = find_in_map(&thm, temperature);
+                    let location = find_in_map(&hlm, humidity);
+                    location
+                })
+                .min()
+        })
+        .min();
+
+    Ok(min.unwrap_or(0))
 }
