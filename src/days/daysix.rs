e0ccbedacfff9e52802d4c1eced479c9b0f5ecee
Domasch <magesthomas@gmail.com>
2023-12-06 12:00:37 +0100

    add explicit lifetime

--- src/days/daysix.rs
+++ src/days/daysix.rs
@@ -1,5 +1,3 @@
-use rayon::range;
-
 use super::utils::read_file;
 
 #[derive(Debug, Clone)]
@@ -10,42 +8,43 @@ struct RaceToBeat {
 
 pub(crate) fn daysix1(filename: &str) -> Result<i32, String> {
     let lines = read_file(filename).map_err(|_| "Reading File")?;
-    let races_to_beat = races_from_lines(&lines);
+    let races_to_beat = races_from_lines(&lines)?;
     Ok(races_to_beat
         .map(|race| {
             (0..=race.time)
                 .map(|time_held| time_held * (race.time - time_held))
                 .filter(|&d_traveled| d_traveled > race.distance)
-                .fold(0, |acc, _| acc + 1)
+                .count() as i32
         })
         .product())
 }
 
-fn races_from_lines(input: &Vec<String>) -> impl Iterator<Item = RaceToBeat> + '_ {
-    let time_iter = input
+fn races_from_lines<'vl>(
+    input: &'vl Vec<String>,
+) -> Result<impl Iterator<Item = RaceToBeat> + 'vl, String> {
+    let time_str_iter = input
         .iter()
         .nth(0)
-        .unwrap()
+        .ok_or("Error reading times")?
         .split(" ")
         .filter(|s| !s.is_empty());
-    let distance_iter = input
+    let distance_str_iter = input
         .iter()
         .nth(1)
-        .unwrap()
+        .ok_or("Error reading distances")?
         .split(" ")
         .filter(|s| !s.is_empty());
 
-    let races_to_beat =
-        time_iter
-            .skip(1)
-            .zip(distance_iter.skip(1))
-            .filter_map(|(t_str, d_str)| {
-                Some(RaceToBeat {
-                    time: t_str.parse().ok()?,
-                    distance: d_str.parse().ok()?,
-                })
-            });
-    races_to_beat
+    let races_to_beat = time_str_iter
+        .skip(1)
+        .zip(distance_str_iter.skip(1))
+        .filter_map(|(t_str, d_str)| {
+            Some(RaceToBeat {
+                time: t_str.parse().ok()?,
+                distance: d_str.parse().ok()?,
+            })
+        });
+    Ok(races_to_beat)
 }
 
 pub(crate) fn daysix2(filename: &str) -> Result<i32, String> {
