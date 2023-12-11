3a826ff294812d4f8f8d719078bf7818ce39a46b
Domasch <magesthomas@gmail.com>
2023-12-11 20:04:00 +0100

    day9 2

--- src/days/daynine.rs
+++ src/days/daynine.rs
@@ -11,20 +11,35 @@ pub(crate) fn daynine1(filepath: &str) -> Result<i32, String> {
             .filter_map(|num_str| num_str.parse::<i32>().ok())
     });
 
-    let difference_sequences = initial_seq.map(|sequence| last_differences(sequence.collect_vec()));
+    let last_diff_sq = initial_seq.map(|sequence| last_differences(sequence.collect_vec()));
     let mut sum: i32 = 0;
-    for mut sequence in difference_sequences {
+    for mut sequence in last_diff_sq {
         sequence.reverse();
         for i in 1..sequence.len() {
             sequence[i] += sequence[i - 1];
+        }
+        sum += sequence.last().unwrap();
+    }
+    Ok(sum)
+}
+pub(crate) fn daynine2(filepath: &str) -> Result<i32, String> {
+    let lines = read_file(filepath).map_err(|_| "Error reading file")?;
+    let initial_seq = lines.iter().map(|line| {
+        line.split_whitespace()
+            .filter_map(|num_str| num_str.parse::<i32>().ok())
+    });
+
+    let last_diff_sq = initial_seq.map(|sequence| first_differences(sequence.collect_vec()));
+    let mut sum: i32 = 0;
+    for mut sequence in last_diff_sq {
+        sequence.reverse();
+        for i in 1..sequence.len() {
+            sequence[i] -= sequence[i - 1];
         }
 
         sum += sequence.last().unwrap();
     }
     Ok(sum)
-}
-pub(crate) fn daynine2(filepath: &str) -> Result<u32, String> {
-    Ok(0)
 }
 fn last_differences<T>(sequence: Vec<T>) -> Vec<T>
 where
@@ -39,5 +54,21 @@ where
         (!diff_seq.is_empty() && diff_seq.iter().any(|&x| x != T::from(0_u8))).then(|| diff_seq)
     })
     .filter_map(|seq| seq.last().copied())
+    .collect()
+}
+
+fn first_differences<T>(sequence: Vec<T>) -> Vec<T>
+where
+    T: std::ops::Sub<Output = T> + Copy + PartialEq + From<u8>,
+{
+    iter::successors(Some(sequence), |seq| {
+        let diff_seq: Vec<T> = seq
+            .iter()
+            .tuple_windows()
+            .map(|(&this, &next)| next - this)
+            .collect();
+        (!diff_seq.is_empty() && diff_seq.iter().any(|&x| x != T::from(0_u8))).then(|| diff_seq)
+    })
+    .filter_map(|seq| seq.first().copied())
     .collect()
 }
